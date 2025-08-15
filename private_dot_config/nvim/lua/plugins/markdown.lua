e2e2230a0b42b71189d3cb9ab6ef302ce04b488f
Daniel Kinzler <kinzler.daniel@gmail.com>
2025-08-15 13:45:34 +0200

    lazy load nvim markdown plugin

--- private_dot_config/nvim/lua/plugins/markdown.lua
+++ private_dot_config/nvim/lua/plugins/markdown.lua
@@ -1,7 +1,11 @@
 return {
     {
         'MeanderingProgrammer/render-markdown.nvim',
+        lazy = true,
+        ft = { 'markdown' },
         dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-        opts = {},
+        opts = {
+            enabled = false,
+        },
     }
 }
