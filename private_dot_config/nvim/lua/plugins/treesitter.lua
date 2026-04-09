6a95d7e8fd81976750cc12efe72be6dd93f3dc79
Daniel Kinzler <kinzler.daniel@gmail.com>
2026-04-09 19:01:58 +0200

    nvim: nvim-treesitter autoinstall

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -5,8 +5,16 @@ return {
         lazy = false,
         build = ":TSUpdate",
         init = function()
-            vim.api.nvim_create_autocmd('FileType', {
+            vim.api.nvim_create_autocmd("FileType", {
                 callback = function()
+                    -- autoinstall missing parsers
+                    local treesitter = require("nvim-treesitter")
+                    local ft = vim.bo.filetype
+                    if not vim.list_contains(treesitter.get_installed(), ft) then
+                        if vim.list_contains(treesitter.get_available(), ft) then
+                            treesitter.install(ft):wait()
+                        end
+                    end
                     -- Enable treesitter highlighting and disable regex syntax
                     -- use pcall to suppress error messages on unsupported file types
                     -- e.g. otherwise opening telescope would throw errors
